025394dd41c1083b6132612f06e5f56f4fb2a104
RanopRamah <101007427+RanopRamah@users.noreply.github.com>
2025-03-06 23:22:49 +0700

    fetch: fetch data from api

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,26 +2,57 @@ import axios from "axios";
 import {useState, useEffect} from "react";
 
 const UserList = () => {
-    const [user, setUser] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        axios.get("http://127.0.0.1:8000/api/users")
-        .then((response) => {
-            setUser(response.data);
-            setLoading(false);
-        })
-        .catch((error) => {
-            console.error("Error fetching data.", error);
-            setLoading(false);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/users")
+      .then((response) => {
+        setUser(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data.", error);
+        setLoading(false);
+      });
+  }, []);
 
-    return(
-        <div>
-        <h2>List User</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>List User</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table border="1" cellPadding="10">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Password</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.map((item) => {
+                return (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.username}</td>
+                        <td>{item.password}</td>
+                        <td>
+                            <button onClick={() => alert(`Lihat detail ${item.id}`)}>
+                                Lihat
+                            </button>
+                        </td>
+                    </tr>
+                );
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
 export default UserList;
